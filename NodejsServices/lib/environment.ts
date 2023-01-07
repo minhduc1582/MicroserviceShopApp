0025bbdf7e7104a227ac4dec49eb12c70d04f975
minhduc1582 <minhduc2247@gmail.com>
2023-01-07 10:31:12 +0700

    + fix struct project

---
+++ NodejsServices/lib/environment.ts
@@ -0,0 +1,57 @@
+enum Environments {
+    local_environment = 'local',
+    dev_environment = 'dev',
+    prod_environment = 'prod',
+    qa_environment = 'qa'
+}
+
+class Environment {
+    private environment: String;
+
+    constructor(environment: String) {
+        this.environment = environment;
+    }
+
+    getPort(): Number {
+        if (this.environment === Environments.prod_environment) {
+            return 8081;
+        } else if (this.environment === Environments.dev_environment) {
+            return 8082;
+        } else if (this.environment === Environments.qa_environment) {
+            return 8083;
+        } else {
+            return 3000;
+        }
+    }
+
+    getDBName(): String {
+        if (this.environment === Environments.prod_environment) {
+            return 'db_test_project_prod';
+        } else if (this.environment === Environments.dev_environment) {
+            return 'db_test_project_dev';
+        } else if (this.environment === Environments.qa_environment) {
+            return 'db_test_project_qa';
+        } else {
+            return 'QTSC_New';
+        }
+    }
+    getUrlRoot(): String{
+        if (this.environment === Environments.prod_environment) {
+            return '';
+        } else if (this.environment === Environments.dev_environment) {
+            return 'https://localhost:44396';
+        } else if (this.environment === Environments.qa_environment) {
+            return '';
+        } else {
+            return 'https://localhost:44396';
+        }
+    }
+    getUrlApcept(): Array<String> {
+        return ["https://www.facebook.com/","https://vntalking.com/"];
+    }
+    getApiKey(): String{
+        return "euzIuxpxUCXEKxEFNlUboVR1kRny3MBAF0LX6BkUn7rlrXnVCS1E2hd0QQ4HbstfuhtOuLpN8NGnKgzraxYceSzk6Wxm7QrnQRdV"
+    }
+}
+
+export default new Environment(Environments.local_environment);
